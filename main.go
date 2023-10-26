47200b7b8debf4272e8118d98ac85a7d568aed23
Nyamu Wanyoike <nyamu.0101@gmail.com>
2023-10-27 00:51:22 +0300

    HTMX x Go Server create go module by running go mod init import htmx via cdn style using tailwind implement add book handler function

--- main.go
+++ main.go
@@ -29,7 +29,18 @@ func main() {
 		templ.Execute(w, books)
 	}
 
+	addBookHandler := func(w http.ResponseWriter, r *http.Request) {
+		title := r.PostFormValue("title")
+		author := r.PostFormValue("author")
+		htmlStr := fmt.Sprintf("<div class='font-bold border p-2 mb-2 border-gray-400 shadow-md flex hover:bg-white hover:cursor-pointer'> <div class='mr-auto'> <p>%s</p> <p class='text-xs text-gray-400'>%s</p> </div> </div>", title, author)
+
+		template, _ := template.New("newBook").Parse(htmlStr)
+
+		template.Execute(w, nil)
+	}
+
 	http.HandleFunc("/", handler1)
+	http.HandleFunc("/add-book/", addBookHandler)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
